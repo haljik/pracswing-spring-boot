f179f1830eb9a03e7946a2a8346937e22de57f34
irof <irof@hogedriven.net>
2015-03-01 12:45:55 +0900

    cas連携させてみる

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,11 +1,25 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
+import org.jasig.cas.client.validation.TicketValidator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.cas.ServiceProperties;
+import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
+import org.springframework.security.cas.authentication.CasAuthenticationProvider;
+import org.springframework.security.cas.web.CasAuthenticationEntryPoint;
+import org.springframework.security.cas.web.CasAuthenticationFilter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebMvcSecurity
@@ -13,25 +27,69 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/top", "/page2", "/login").permitAll()
+        http.addFilter(casFilter());
+        http.exceptionHandling().authenticationEntryPoint(casEntryPoint());
+
+        http.authorizeRequests()
+                .antMatchers("/", "/top", "/page2").permitAll()
                 .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .failureHandler(new RequestParameterUrlAuthenticationFailureHandler("/login", "loginPage"))
-                .and()
+            .and()
             .logout()
                 .logoutSuccessUrl("/top")
                 .permitAll();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("pass").roles("USER");
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(casAuthenticationProvider());
+    }
+
+    @Bean
+    public ServiceProperties serviceProperties() {
+        ServiceProperties serviceProperties = new ServiceProperties();
+        serviceProperties.setService("https://localhost:8443/j_spring_cas_security_check");
+        serviceProperties.setSendRenew(false);
+        return serviceProperties;
+    }
+
+    @Bean
+    public CasAuthenticationFilter casFilter() throws Exception {
+        CasAuthenticationFilter filter = new CasAuthenticationFilter();
+        filter.setAuthenticationManager(authenticationManager());
+        return filter;
+    }
+
+    @Bean
+    public CasAuthenticationEntryPoint casEntryPoint() {
+        CasAuthenticationEntryPoint entryPoint = new CasAuthenticationEntryPoint();
+        entryPoint.setLoginUrl("https://localhost:9443/login");
+        entryPoint.setServiceProperties(serviceProperties());
+        return entryPoint;
+    }
+
+    @Bean
+    public CasAuthenticationProvider casAuthenticationProvider() {
+        CasAuthenticationProvider casAuthenticationProvider = new CasAuthenticationProvider();
+        casAuthenticationProvider.setAuthenticationUserDetailsService(authenticationUserDetailsService());
+        casAuthenticationProvider.setServiceProperties(serviceProperties());
+        casAuthenticationProvider.setTicketValidator(ticketValidator());
+        casAuthenticationProvider.setKey("some_id_for_this_cas_prov");
+        return casAuthenticationProvider;
+    }
+
+    @Bean
+    public TicketValidator ticketValidator() {
+        return new Cas20ServiceTicketValidator("https://localhost:9443");
+    }
+
+    @Bean
+    public AuthenticationUserDetailsService<CasAssertionAuthenticationToken> authenticationUserDetailsService() {
+        return new UserDetailsByNameServiceWrapper<>(userDetailsServiceBean());
+    }
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() {
+        return username -> new User(username, "",
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
     }
 }
