65b0ad62cbc0dc7ee1480736cebc10a3742a9f3d
haljik <se.k.hal7700@gmail.com>
2015-04-27 03:09:39 +0900

    cacertsファイルをbuildディレクトリに生成するようにした。

--- bootRunWithCas.sh
+++ bootRunWithCas.sh
@@ -1,4 +1,4 @@
-rm ./cacerts
-keytool -importcert -trustcacerts -alias localcas -keystore ./cacerts -file ../run-cas-jetty/LocalCAS.crt -storepass localcas -noprompt
+./gradlew clean compileJava
+keytool -importcert -trustcacerts -alias localcas -keystore ./build/cacerts -file ../run-cas-jetty/LocalCAS.crt -storepass localcas -noprompt
 export _JAVA_OPTIONS="-Djavax.net.ssl.trustStore=/Users/haljik/dev/system-sekkei/pracswing-spring-boot/cacerts -Djavax.net.ssl.trustStorePassword=localcas"
 ./gradlew bootRun
